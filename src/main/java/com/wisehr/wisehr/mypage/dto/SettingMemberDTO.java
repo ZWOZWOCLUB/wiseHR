d92ce54ab02a7261b7cdc6c6bb94a8bd79af6451
DagyeongKang <gangda920@gmail.com>
2024-01-24 15:02:43 +0900

    샘플데이터 후 mypage 시

---
+++ src/main/java/com/wisehr/wisehr/mypage/dto/SettingMemberDTO.java
@@ -0,0 +1,23 @@
+package com.wisehr.wisehr.mypage.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class SettingMemberDTO {
+    private String memName;
+    private String memPhone;
+    private String memEmail;
+    private String memAddress;
+    private String memBirth;
+    private String memPassword;
+    private String memHireDate;
+    private String memEndDate;
+    private String memStatus;
+    private String memRoll;
+
+
+}
