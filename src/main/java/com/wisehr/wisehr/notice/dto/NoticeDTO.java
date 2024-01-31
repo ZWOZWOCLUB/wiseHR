35e56a927c2f2e966855c7212d498d84a70e5075
DONGROCK <ehdfkr8022@naver.com>
2024-01-31 20:28:28 +0900

    공지 등록 첨부파일 추가중 크리에이트데이트에서 문제 발생

--- src/main/java/com/wisehr/wisehr/notice/dto/NoticeDTO.java
+++ src/main/java/com/wisehr/wisehr/notice/dto/NoticeDTO.java
@@ -1,5 +1,6 @@
 package com.wisehr.wisehr.notice.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.util.Date;
@@ -14,6 +15,7 @@ public class NoticeDTO {
     private String notName;
     private String notComment;  //내용
     private Long notView;       //조회수
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+9")
     private Date notCreateDate; //작성일
     private Date notModifyDate; //수정일
     private NotMemberDTO notMember; //작성자
