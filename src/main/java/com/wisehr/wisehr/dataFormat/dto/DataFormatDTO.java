f0a2aa1de3920c80e44435345888e42c800ca0fc
DONGROCK <ehdfkr8022@naver.com>
2024-02-03 17:39:40 +0900

    서식자료실 post 성공 조회 시작

---
+++ src/main/java/com/wisehr/wisehr/dataFormat/dto/DataFormatDTO.java
@@ -0,0 +1,22 @@
+package com.wisehr.wisehr.dataFormat.dto;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+public class DataFormatDTO {
+    private Long dataCode;
+    private String dataName;
+    private Long memCode;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date registDate;
+    private Long dataSize;
+    private String dataPath;
+    private String dataDeleteStatus;
+}
