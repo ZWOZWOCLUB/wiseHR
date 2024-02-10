39f5d02c2b3a53035ce5cefaa883e4c8427f2f43
DONGROCK <ehdfkr8022@naver.com>
2024-02-11 03:45:17 +0900

    전체조회 test

--- src/main/java/com/wisehr/wisehr/notice/repository/NoticeRepository.java
+++ src/main/java/com/wisehr/wisehr/notice/repository/NoticeRepository.java
@@ -1,10 +1,14 @@
 package com.wisehr.wisehr.notice.repository;
 
 import com.wisehr.wisehr.notice.entity.Notice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface NoticeRepository extends JpaRepository<Notice, String> {
 
     //공지제목조회
@@ -13,4 +17,9 @@ public interface NoticeRepository extends JpaRepository<Notice, String> {
     List<Notice> findByNotCommentContaining(String search);
     //공지작성자조회
     List<Notice> findByNotMemberMemNameContaining(String search);
+
+    @Query(value = "SELECT * FROM notice ORDER BY CAST(SUBSTRING(notCode, 4) AS UNSIGNED) ASC",
+            countQuery = "SELECT count(*) FROM notice",
+            nativeQuery = true)
+    Page<Notice> findAllWithCustomOrder(Pageable pageable);
 }
