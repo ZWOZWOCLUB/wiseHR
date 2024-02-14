c440e4c1467a46ee2e9c17a25befc808ef31601c
DONGROCK <ehdfkr8022@naver.com>
2024-02-14 22:10:17 +0900

    공지 상세조회 댓글 추가해야함. 첨부파일저장시 확장자출력되는거 수정해야함. 상세페이지 댓글중간저장

--- src/main/java/com/wisehr/wisehr/security/common/ZzclubRole.java
+++ src/main/java/com/wisehr/wisehr/security/common/ZzclubRole.java
@@ -1,10 +1,10 @@
 package com.wisehr.wisehr.security.common;
 
 public enum ZzclubRole {
-    일반사원("USER"),
-    중간관리자("ADMIN"),
-    최고관리자("SUPER"),
-    전체("USER, ADMIN, SUPER");
+    USER("USER"),
+    ADMIN("ADMIN"),
+    SUPERADMIN("SUPERADMIN"),
+    ALL("USER, ADMIN, SUPER");
 
 
     private final String memRole;
