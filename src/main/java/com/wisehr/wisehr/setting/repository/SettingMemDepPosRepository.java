99605f9f21c590b946b1a20071773a958e6459d3
yeonji-0120 <noyeonji43@gmail.com>
2024-02-23 18:21:47 +0900

    2024.02.23 오류 수정

--- src/main/java/com/wisehr/wisehr/setting/repository/SettingMemDepPosRepository.java
+++ src/main/java/com/wisehr/wisehr/setting/repository/SettingMemDepPosRepository.java
@@ -1,13 +1,15 @@
 package com.wisehr.wisehr.setting.repository;
 
 import com.wisehr.wisehr.setting.entity.SettingMemDepPos;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface SettingMemDepPosRepository extends JpaRepository <SettingMemDepPos, Integer> {
-    List<SettingMemDepPos> findByMemNameContaining(String search);
+    List<SettingMemDepPos> findByMemNameContainingAndMemStatus(String search, String memStatus);
 
 
 }
